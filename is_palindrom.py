6c2339461bd3c9e8e916e5cccbe0c31445f2e095
need-for-coding <37559200+need-for-coding@users.noreply.github.com>
2022-04-17 11:26:27 +0300

    Create is_palindrom.py
    Check a number as the palindrom

---
+++ is_palindrom.py
@@ -0,0 +1,14 @@
+def is_palindrom(num):
+    if num // 10 == 0:
+        return False
+    temp = num
+    reversed_num = 0
+    
+    while temp != 0:
+        reversed_num = (reversed_num * 10) + (temp % 10)
+        temp = temp // 10
+        
+    if num == reversed_num:
+        return num
+    else:
+        return False
